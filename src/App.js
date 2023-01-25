350bb59b212f64404185ce8be32951fdb8a30426
Marco Carraro <carra.rules@gmail.com>
2023-01-25 19:04:33 +0100

    Create the first components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Die from './components/Die'
+import { useState } from 'react';
+
 
 function App() {
+  function allNewDice() { 
+    const newDice = []
+    for (let i = 0; i < 10; i++ ) {
+      const die = Math.ceil(Math.random() * 6)
+      newDice.push(die)
+    }
+    return newDice
+  }
+  // console.log(allNewDice())
+
+  function rollDice() {
+    setDice(allNewDice)
+  }
+
+  const [dice, setDice] = useState(allNewDice())
+  const diceElement = dice.map(die => {
+    return <Die value={die} />
+    }   
+  )
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <main>
+      <div className="dice-container">
+        {diceElement}
+      </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
+    </main>
+
   );
 }
 
---
+++ src/components/Die.js
@@ -0,0 +1,9 @@
+function Die(props) {
+    return (
+        <div className="die-face">
+            <h2 className="dice-num">{ props.value }</h2>
+        </div>
+        );
+}
+
+export default Die;
